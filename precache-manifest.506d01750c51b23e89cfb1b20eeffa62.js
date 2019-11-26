225dcc2ad8869062e3b61c86e41cfef59d173e6d
uahmed23 <umer@uoguelph.ca>
2019-11-26 00:21:30 -0500

    Updates

---
+++ precache-manifest.506d01750c51b23e89cfb1b20eeffa62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d5a88bdc5fb09541312dcc2c2af3ed",
+    "url": "/workout-routine-planning-app/index.html"
+  },
+  {
+    "revision": "811133c6dc2e0b0082c4",
+    "url": "/workout-routine-planning-app/static/css/main.2be0cb4a.chunk.css"
+  },
+  {
+    "revision": "9c4d1bc36205b8f51d69",
+    "url": "/workout-routine-planning-app/static/js/2.b6d0a128.chunk.js"
+  },
+  {
+    "revision": "811133c6dc2e0b0082c4",
+    "url": "/workout-routine-planning-app/static/js/main.d6932710.chunk.js"
+  },
+  {
+    "revision": "6a0d2137732d53e8fda5",
+    "url": "/workout-routine-planning-app/static/js/runtime-main.1c593353.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/workout-routine-planning-app/static/media/logo.25bf045c.svg"
+  }
+]);
